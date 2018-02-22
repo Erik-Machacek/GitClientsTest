dde6daca7ba954d5f646a64d3e8bf5259314f756
EMachacek <emachacek@eprin.cz>
2018-02-22 22:14:16 +0100

    [Added] Info about program starting

--- GitClientsTest/Program.cs
+++ GitClientsTest/Program.cs
@@ -6,6 +6,7 @@ namespace GitClientsTest
   {
     private static void Main(string[] args)
     {
+      Console.Write("Program started!");
       Console.WriteLine("Press any key to close program...");
       Console.ReadKey();
     }
